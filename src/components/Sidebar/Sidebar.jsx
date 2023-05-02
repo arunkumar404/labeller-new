cc5f9237a0cbb6324d7eb3c690b7c045f978dce7
Arun kumar <aravi@purecodesoftware.com>
2023-05-02 18:47:00 +0530

    draggable sidebar added

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
 import ElementsGroupFilter from "./ElementsGroupFilter";
 import { useElementsContext } from "../../context";
@@ -6,10 +6,35 @@ import ElementsIndividualFilter from "./ElementsIndividualFilter";
 import LabelInput from "./LabelInput";
 const Sidebar = () => {
   const { selectedSidebar,currentHighlightedElement } = useElementsContext();
+
+  const [isDragging, setIsDragging] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [dragStart, setDragStart] = useState({ x: 0, y: 0 });
+
+  const handleMouseDown = (event) => {
+    setIsDragging(true);
+    setDragStart({ x: event.clientX, y: event.clientY });
+  };
+
+  const handleMouseMove = (event) => {
+    if (!isDragging) return;
+    const dx = event.clientX - dragStart.x;
+    setPosition({ x: position.x - dx, y: 0 });
+    setDragStart({ x: event.clientX, y: event.clientY });
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
   return (
     <div
-      style={{ display: `${!selectedSidebar ? "none" : ""}` }}
+      style={{ display: `${!selectedSidebar ? "none" : ""}`,right:position.x }}
       className="sidebar"
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={()=>setIsDragging(false)}
     >
       {selectedSidebar === "group_filter" && <ElementsGroupFilter />}
       {selectedSidebar === "individual_filter" && <ElementsIndividualFilter />}
